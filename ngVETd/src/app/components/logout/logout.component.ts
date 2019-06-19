0fef1aaf813449624d53521bc8786f5e4bddd820
aveigel <alexaveigel@yahoo.com>
2019-06-19 16:52:43 -0600

    navbar

--- ngVETd/src/app/components/logout/logout.component.ts
+++ ngVETd/src/app/components/logout/logout.component.ts
@@ -1,15 +1,29 @@
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
 
 @Component({
-  selector: 'app-logout',
-  templateUrl: './logout.component.html',
-  styleUrls: ['./logout.component.css']
+  selector: "app-logout",
+  templateUrl: "./logout.component.html",
+  styleUrls: ["./logout.component.css"]
 })
 export class LogoutComponent implements OnInit {
+  //
+  // F E I L D S
+  //
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) {}
 
-  ngOnInit() {
+  //
+  // M E T H O D S
+  //
+
+  ngOnInit() {}
+
+  logout() {
+    console.log("logout");
+    this.auth.logout();
+    this.router.navigateByUrl("home");
   }
-
 }
