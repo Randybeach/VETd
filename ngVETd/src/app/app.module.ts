1574f1393894cb3fd49e0f45e3bccb82d3ea52a4
aveigel <alexaveigel@yahoo.com>
2019-06-19 14:03:13 -0600

    Stubbed components and auth service

--- ngVETd/src/app/app.module.ts
+++ ngVETd/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    LogoutComponent,
+    NavigationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
