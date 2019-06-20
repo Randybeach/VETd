381b606b1a812ba237c83f75bcbc43759b261504
Rob Lounsbury <rob.lounsbury@gmail.com>
2019-06-20 11:33:44 -0600

    home background image and initial type

--- ngVETd/src/app/app.module.ts
+++ ngVETd/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthService } from './services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
-import { CarouselModule } from 'angular-bootstrap-md'
+import { CarouselModule } from 'angular-bootstrap-md';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
