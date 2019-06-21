07487be95a531bcd6b80b310f2c509897283be52
aveigel <alexaveigel@yahoo.com>
2019-06-21 13:23:23 -0600

    login lout out functions work

--- ngVETd/src/app/components/profile/profile.component.ts
+++ ngVETd/src/app/components/profile/profile.component.ts
@@ -29,6 +29,7 @@ export class ProfileComponent implements OnInit {
   ngOnInit() {
     this.reloadSectors();
     this.reloadJobs();
+    console.log(this.reloadSectors());
   }
 
   reloadSectors() {
