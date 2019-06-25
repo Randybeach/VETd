dd3f07fa63e3fd77bd36fda5b9d1905613af8a59
Rob Lounsbury <rob.lounsbury@gmail.com>
2019-06-25 10:22:19 -0600

    resolved modal merge conflict

--- ngVETd/src/app/DialogData.ts
+++ ngVETd/src/app/DialogData.ts
@@ -3,10 +3,7 @@ import { Message } from './models/message';
 
 export interface DialogData {
   profile: Profile;
-<<<<<<< HEAD
   myprofile: Profile;
   messages: Message[];
-=======
-  myProfile: Profile;
->>>>>>> 56d8162854b3d4022c29b581e492a0c6f238e37d
+
 }
